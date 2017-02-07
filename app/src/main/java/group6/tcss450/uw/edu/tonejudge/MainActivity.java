1080e0ef506a9ac56393bcc0a16b9609ba9654bb
hunterwb <hunterwb@washington.edu>
2017-02-07 11:00:57 -0800

    registration with server

--- app/src/main/java/group6/tcss450/uw/edu/tonejudge/MainActivity.java
+++ app/src/main/java/group6/tcss450/uw/edu/tonejudge/MainActivity.java
@@ -1,5 +1,6 @@
 package group6.tcss450.uw.edu.tonejudge;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -27,8 +28,10 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(intent);
             }
         });
 
@@ -85,8 +88,6 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
 
         } else if (id == R.id.nav_share) {
 
--- app/src/main/java/group6/tcss450/uw/edu/tonejudge/RegisterActivity.java
+++ app/src/main/java/group6/tcss450/uw/edu/tonejudge/RegisterActivity.java
@@ -1,45 +1,126 @@
 package group6.tcss450.uw.edu.tonejudge;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
+import android.content.ContentValues;
 import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
 
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
 import android.os.AsyncTask;
 
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.text.TextUtils;
-import android.view.KeyEvent;
+import android.util.Log;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-import static android.Manifest.permission.READ_CONTACTS;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
-public class RegisterActivity extends AppCompatActivity {
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText emailView;
+    private EditText passwordView;
+    private EditText passwordConfirmView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        emailView = (EditText) findViewById(R.id.register_email);
+        passwordView = (EditText) findViewById(R.id.register_password);
+        passwordConfirmView = (EditText) findViewById(R.id.register_password_confirm);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String email = emailView.getText().toString();
+        String password = passwordView.getText().toString();
+        String passwordConfrim = passwordConfirmView.getText().toString();
+        boolean pass = true;
+        if (email.isEmpty()) {
+            emailView.setError("Email is required");
+            pass = false;
+        }
+        if (!email.isEmpty() && !email.contains("@")) {
+            emailView.setError("Invalid email");
+            pass = false;
+        }
+        if (password.isEmpty()) {
+            passwordView.setError("Password is required");
+            pass = false;
+        }
+        if (passwordConfrim.isEmpty()) {
+            passwordConfirmView.setError("Password confirmation is required");
+            pass = false;
+        }
+        if (!password.equals(passwordConfrim)) {
+            passwordConfirmView.setError("Passwords do not match");
+            pass = false;
+        }
+        if (pass) {
+            new Register().execute(email, password);
+        }
+    }
+
+    private class Register extends AsyncTask<String, Void, String> {
+
+        private final String URL_STRING = "https://xk6ntzqxr2.execute-api.us-west-2.amazonaws.com/tonejudge/users";
+        private final String ACTION = "register";
+
+        @Override
+        protected String doInBackground(String... params) {
+            String email = params[0];
+            String password = params[1];
+            MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+            OkHttpClient client = new OkHttpClient();
+            JSONObject jsonBody = new JSONObject();
+            try {
+                jsonBody.put("action", ACTION);
+                jsonBody.put("email", email);
+                jsonBody.put("password", password);
+            } catch (JSONException e) {
+                return e.getMessage();
+            }
+            Log.d(getClass().getSimpleName(), jsonBody.toString());
+            Request request = new Request.Builder()
+                    .url(URL_STRING)
+                    .post(RequestBody.create(JSON, jsonBody.toString()))
+                    .build();
+            try {
+                Response response = client.newCall(request).execute();
+                Log.d(getClass().getSimpleName(), response.toString());
+                JSONObject r = new JSONObject(response.body().string());
+                Log.d(getClass().getSimpleName(), r.toString());
+                if (r.has("errorMessage")) {
+                    return r.getString("errorMessage");
+                }
+            } catch (IOException | JSONException e) {
+                return e.getMessage();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            if (s == null) {
+                Toast.makeText(getApplicationContext(), "Registered Successfully", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
 
