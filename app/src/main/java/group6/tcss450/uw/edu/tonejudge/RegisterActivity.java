17fed2081e16c9591cea3cfaacb42475968f55d6
hunterwb <hunterwb@washington.edu>
2017-02-11 00:09:11 -0800

    delete unused files, fix dependencies

--- app/src/main/java/group6/tcss450/uw/edu/tonejudge/RegisterActivity.java
+++ app/src/main/java/group6/tcss450/uw/edu/tonejudge/RegisterActivity.java
@@ -2,7 +2,6 @@ package group6.tcss450.uw.edu.tonejudge;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 
 import android.os.AsyncTask;
