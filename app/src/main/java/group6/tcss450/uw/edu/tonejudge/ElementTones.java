c98e112c6f3a697cd8f9acc67c5c44596d7bf162
hunterwb <hunterwb@washington.edu>
2017-02-26 23:06:34 -0800

    top ranks tone page requests

--- app/src/main/java/group6/tcss450/uw/edu/tonejudge/ElementTones.java
+++ app/src/main/java/group6/tcss450/uw/edu/tonejudge/ElementTones.java
@@ -7,6 +7,9 @@ import com.ibm.watson.developer_cloud.tone_analyzer.v3.model.ToneScore;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ElementTones {
 
     public static JSONObject elementToneToDbJson(ElementTone elementTone) {
@@ -21,5 +24,31 @@ public class ElementTones {
             e.printStackTrace();
         }
         return json;
+    }
+
+    public static ElementTone dbJsonToElementTone(JSONObject json) {
+        ElementTone elementTone = new ElementTone();
+        try {
+            List<ToneCategory> toneCategories = new ArrayList<>();
+            for (Tone.Category category : Tone.Category.values()) {
+                ToneCategory toneCategory = new ToneCategory();
+                toneCategory.setName(category.getName());
+                toneCategory.setId(category.getId());
+                List<ToneScore> tones = new ArrayList<>();
+                for (Tone tone : category.getTones()) {
+                    ToneScore toneScore = new ToneScore();
+                    toneScore.setScore(json.getDouble(tone.getId()));
+                    toneScore.setId(tone.getId());
+                    toneScore.setName(tone.getName());
+                    tones.add(toneScore);
+                }
+                toneCategory.setTones(tones);
+                toneCategories.add(toneCategory);
+            }
+            elementTone.setTones(toneCategories);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return elementTone;
     }
 }
--- app/src/main/java/group6/tcss450/uw/edu/tonejudge/TopRanksToneActivity.java
+++ app/src/main/java/group6/tcss450/uw/edu/tonejudge/TopRanksToneActivity.java
@@ -3,17 +3,83 @@ package group6.tcss450.uw.edu.tonejudge;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+
+import com.ibm.watson.developer_cloud.tone_analyzer.v3.model.ElementTone;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TopRanksToneActivity extends NavDrawerActivity {
 
     private Tone mTone;
+    private int mPage;
+    private ListView mList;
+
+    private List<ElementTone> mElementTones = new ArrayList<>();
+    private List<String> mTexts = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_ranks_tone);
         mTone = (Tone) getIntent().getSerializableExtra("tone");
+        mPage = 0;
         ((Toolbar) findViewById(R.id.toolbar)).setTitle("Top: " + mTone.getName());
+        mList = (ListView) findViewById(R.id.top_ranks_tone_list);
         onCreateDrawer();
+        loadNextPage();
+    }
+
+    public void loadNextPage() {
+        try {
+            JSONObject request = new JSONObject();
+            request.put("page", mPage);
+            request.put("tone", mTone.getId());
+            request.put("action", PageTask.ACTION);
+            new PageTask().execute(request);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private class PageTask extends JsonPostTask {
+
+        private static final String ACTION = "top";
+
+        public PageTask() {
+            super("https://xk6ntzqxr2.execute-api.us-west-2.amazonaws.com/tonejudge/results");
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject jsonObject) {
+            super.onPostExecute(jsonObject);
+            Log.d("", jsonObject.toString());
+            try {
+                JSONArray results = jsonObject.getJSONArray("results");
+                for (int i = 0; i < results.length(); i++) {
+                    JSONObject result = results.getJSONObject(i);
+                    mTexts.add(result.getString("text"));
+                    mElementTones.add(ElementTones.dbJsonToElementTone(result));
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            mPage++;
+            Log.d("", mElementTones.toString());
+            Log.d("", mTexts.toString());
+        }
     }
 }
