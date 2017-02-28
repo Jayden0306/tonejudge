7008238825f89a439c7be0118c9b7fada01ac82b
hunterwb <hunterwb@washington.edu>
2017-02-27 17:10:17 -0800

    top ranks tone cardview

--- app/src/main/java/group6/tcss450/uw/edu/tonejudge/TopRanksToneActivity.java
+++ app/src/main/java/group6/tcss450/uw/edu/tonejudge/TopRanksToneActivity.java
@@ -1,12 +1,15 @@
 package group6.tcss450.uw.edu.tonejudge;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ListView;
-import android.widget.ProgressBar;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.ibm.watson.developer_cloud.tone_analyzer.v3.model.ElementTone;
 
@@ -19,12 +22,15 @@ import java.util.List;
 
 public class TopRanksToneActivity extends NavDrawerActivity {
 
+    private static int PAGE_SIZE = 25;
+
     private Tone mTone;
     private int mPage;
-    private ListView mList;
-
-    private List<ElementTone> mElementTones = new ArrayList<>();
-    private List<String> mTexts = new ArrayList<>();
+    private RecyclerView mRecycler;
+    private LinearLayoutManager mLayoutManager;
+    private Adapter mAdapter;
+    private PageTask mPageTask;
+    private boolean hasReachedEnd = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +39,22 @@ public class TopRanksToneActivity extends NavDrawerActivity {
         mTone = (Tone) getIntent().getSerializableExtra("tone");
         mPage = 0;
         ((Toolbar) findViewById(R.id.toolbar)).setTitle("Top: " + mTone.getName());
-        mList = (ListView) findViewById(R.id.top_ranks_tone_list);
+        mRecycler = (RecyclerView) findViewById(R.id.top_ranks_tone_recycler);
+        mAdapter = new Adapter();
+        mRecycler.setAdapter(mAdapter);
+        mRecycler.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                int itemCount = mLayoutManager.getItemCount();
+                if (!hasReachedEnd && mPageTask == null &&  itemCount == mLayoutManager.findLastVisibleItemPosition() + 1) {
+                    loadNextPage();
+                }
+            }
+        });
+        mLayoutManager = new LinearLayoutManager(this);
+        mLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        mRecycler.setLayoutManager(mLayoutManager);
         onCreateDrawer();
         loadNextPage();
     }
@@ -42,6 +63,7 @@ public class TopRanksToneActivity extends NavDrawerActivity {
         try {
             JSONObject request = new JSONObject();
             request.put("page", mPage);
+            request.put("pageSize", PAGE_SIZE);
             request.put("tone", mTone.getId());
             request.put("action", PageTask.ACTION);
             new PageTask().execute(request);
@@ -56,11 +78,7 @@ public class TopRanksToneActivity extends NavDrawerActivity {
 
         public PageTask() {
             super("https://xk6ntzqxr2.execute-api.us-west-2.amazonaws.com/tonejudge/results");
-        }
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
+            mPageTask = this;
         }
 
         @Override
@@ -69,17 +87,61 @@ public class TopRanksToneActivity extends NavDrawerActivity {
             Log.d("", jsonObject.toString());
             try {
                 JSONArray results = jsonObject.getJSONArray("results");
+                if (results.length() < PAGE_SIZE) {
+                    hasReachedEnd = true;
+                }
                 for (int i = 0; i < results.length(); i++) {
                     JSONObject result = results.getJSONObject(i);
-                    mTexts.add(result.getString("text"));
-                    mElementTones.add(ElementTones.dbJsonToElementTone(result));
+                    mAdapter.mTexts.add(result.getString("text"));
+                    mAdapter.mResults.add(result);
                 }
+                mAdapter.notifyItemRangeInserted(mAdapter.mTexts.size() - results.length(), results.length());
             } catch (JSONException e) {
                 e.printStackTrace();
             }
             mPage++;
-            Log.d("", mElementTones.toString());
-            Log.d("", mTexts.toString());
+            mPageTask = null;
         }
     }
+
+    private class Holder extends RecyclerView.ViewHolder {
+
+        protected TextView text;
+        protected TextView score;
+
+        public Holder(View itemView) {
+            super(itemView);
+            text = (TextView) itemView.findViewById(R.id.top_ranks_tone_card_text);
+            score = (TextView) itemView.findViewById(R.id.top_ranks_tone_card_score);
+        }
+    }
+
+    private class Adapter extends RecyclerView.Adapter<Holder> {
+
+        private List<JSONObject> mResults = new ArrayList<>();
+        private List<String> mTexts = new ArrayList<>();
+
+        @Override
+        public Holder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.top_ranks_tone_card, parent, false);
+            return new Holder(itemView);
+        }
+
+        @Override
+        public void onBindViewHolder(Holder holder, int position) {
+            holder.text.setText(mTexts.get(position));
+            try {
+                holder.score.setText(Double.toString(mResults.get(position).getDouble(mTone.getId())));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            return mTexts.size();
+        }
+    }
+
+
 }
